5a93ac389da58dc4b723c775ea6b77e0eb32909b
ltteh <67978926+ltteh@users.noreply.github.com>
2021-05-02 23:56:44 +0100

    lvl 500

--- server.js
+++ server.js
@@ -10,24 +10,53 @@ const harrowData = require("./data/Harrow.json")
 const heathrowData = require("./data/Heathrow.json")
 const stratfordData = require("./data/Stratford.json")
 
-app.get("/harrow/", (req, res) => {
-    res.status(200).send(harrowData.nurse);
+// put 3 cities data together in a new array of object
+const allData = {
+    harrow: harrowData,
+    heathrow: heathrowData,
+    stratford: stratfordData
+}
+
+// root path
+app.get("/", (req, res) => {
+    res.status(200).send(allData)
 })
 
-app.get("/harrow/pharmacies", (req, res) => {
-    res.status(200).send(harrowData.pharmacies);
+// select city endpoint
+app.get("/:city", (req, res) => {
+    const selectedCity = req.params.city
+
+    // selected city validation
+    if (!allData[selectedCity]) {
+        res.status(404).send({
+            message: "City not found, select available city"
+        })
+    } else {
+        res.status(200).send({
+            citySelected: allData[selectedCity]
+        })
+    }
 })
 
-app.get("/harrow/colleges", (req, res) => {
-    res.status(200).send({colleges: harrowData.colleges});
-})
+app.get("/:city/:category", (req, res) => {
+    const selectedCity = req.params.city
+    const selectedCategory = req.params.category
 
-app.get("/harrow/doctors", (req, res) => {
-    res.status(200).send(harrowData.doctors);
-})
-
-app.get("/harrow/hospitals", (req, res) => {
-    res.status(200).send(harrowData.hospitals);
+    // selected city validation
+    if (!allData[selectedCity]) {
+        res.status(404).send({
+            message: "City not found, select available city"
+        })
+        // selected category validation
+    } else if (!allData[selectedCity][selectedCategory]) {
+        res.status(404).send({
+            message: "Category not found, select available category"
+        })
+    } else {
+        res.status(200).send({
+            categorySelected: allData[selectedCity][selectedCategory]
+        })
+    }
 })
 
 app.listen(PORT, function() {
